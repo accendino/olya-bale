14f6cf30ca77ff6f2edea2b74bc0c55393eefeb3
Olya Olya <achendina.com>
2017-09-15 01:12:14 +0300

    26. Moved placeholders handle & small screen navigation scripts into pattern module; Form validation script remade into a function; Added icon for map marker.

--- app/js/main.js
+++ app/js/main.js
@@ -1,178 +1,148 @@
 $(document).ready(function(){
 	console.log("User JS");
 
-	var emailField = $("input[name ='email']");
-	var emailErrorMessage = $('div[data-error-message="email"]');
-	var passwordField = $("input[name ='password']");
-	var passwordErrorMessage = $('div[data-error-message="password"]');
+	// 1. SUBMIT ACTION: show and hide ERRORS
+	var submitAction = function(submitObject, field, error) {
 
-	var requiredClass = 'hidden';	
+		// 1.1 FIELD CHECK, SHOW ERROR IF EMPTY
+	    $(submitObject).on("submit", function(e){
 
-	//---------------------------------------------------------------------
-	// FORM SUBMIT ACTION
-	$("#authenticate-form").on("submit", function(e){
+		    var fieldValue = $(field).val();
+		    e.preventDefault();
 
-		var emailFieldValue = $("input[name ='email']").val(); // ""
-		var passwordFieldValue = $("input[name ='password']").val(); // ""
+		    var isValid = false;
+		    if ( fieldValue == "" ){
+			    isValid = false;
+			    $(error).removeClass('hidden');
+		    } else {
+			    isValid = true;
+		    }
+	    });
 
-		// *  *  *  *  *  *
-		e.preventDefault();
-		// *  *  *  *  *  *
-
-		var isValid = false;
-
-		// E-MAIL CHECK
-		if ( emailFieldValue == "" ){
-			isValid = false;
-			emailErrorMessage.addClass("visible");
-		} else {
-			isValid = true;
-		}
-		// *  *  *  *  *  *
-
-		// PASSWORD CHECK
-		isValid = false;
-		if ( passwordFieldValue == "" ){
-			isValid = false;
-			passwordErrorMessage.addClass("visible");
-		} else {
-			isValid = true;
-		}
-		// *  *  *  *  *  *
-
-	});
-	//     *     *     *     *     * 
-
-	      // HIDE ERRORS ON KEYUP
-
-	emailField.on("keyup", function() {
-		if ( emailErrorMessage.hasClass("visible")) {
-		 	emailErrorMessage.removeClass("visible");
-		}
-	});
-
-	passwordField.on("keyup", function() {
-		if ( passwordErrorMessage.hasClass("visible")) {
- 			passwordErrorMessage.removeClass("visible");
-		}
-	});
-
-	//---------------------------------------------------------------------
-	// REVIEW SUBMIT ACTION
-
-	var writeReview = $("form[id = 'writeReview']");
-	var writeReviewTextarea = $("form[id = 'writeReview'] textarea");
-	var writeReviewError = $("form[id = 'writeReview'] .error");
-
-
-	$(writeReview).on("submit", function(e){
-
-		var reviewFieldValue = $(writeReviewTextarea).val();
-		e.preventDefault();
-
-		// REVIEW CHECK
-		var isValid = false;
-		if ( reviewFieldValue == "" ){
-			isValid = false;
-			$(writeReviewError).removeClass("hidden");
-		} else {
-			isValid = true;
-		}
-	});
-	//     *     *     *     *     *
-
-	      // HIDE ERROR ON KEYUP
-
-	$(writeReviewTextarea).on("keyup", function() {
- 		$(writeReviewError).addClass("hidden");
-	});
-
-	//---------------------------------------------------------------------
-	// PLACEHOLDERS / get better!!! :)
-/*
-	$(emailField).on('focus', function(){
-		$(this).attr('placeholder', '');
-	});
-	$(emailField).on('blur', function(){
-		$(this).attr('placeholder', 'E-mail');
-	});
-
-	//     *     *     *     *     * 
-	$(passwordField).on('focus', function(){
-		$(this).attr('placeholder', '');
-	});
-	$(passwordField).on('blur', function(){
-		$(this).attr('placeholder', 'Пароль');
-	});
-
-	//     *     *     *     *     * 
-	$('textarea').on('focus', function(){
-		$(this).attr('placeholder', '');
-	});
-	$('textarea').on('blur', function(){
-		$(this).attr('placeholder', 'Присоединиться к обсуждению...');
-	});
-*/
-
+	    // 1.2 HIDE ERROR ON KEYUP
+	    $(field).on("keyup", function() {
+ 		    $(error).addClass('hidden');
+	    });
     
-    // Improved placeholders Handler
-	var placeHolderSave;
+    };
+
+
+    //CALL FUNCTION FOR E-MAIL FIELD
+    submitAction($("form[id = 'authenticate-form']"), 
+        	     $("input[name ='email']"), 
+        	     $("form[id = 'authenticate-form'] div[data-error-message='email']"));
+    
+    //CALL FUNCTION FOR PASSWORD FIELD
+    submitAction($("form[id = 'authenticate-form']"), 
+        	     $("input[name ='password']"), 
+        	     $("form[id = 'authenticate-form'] div[data-error-message='password']"));
+
+    //CALL FUNCTION FOR REVIEW FIELD
+    submitAction($("form[id = 'writeReview']"), 
+        	     $("form[id = 'writeReview'] textarea"), 
+        	     $("form[id = 'writeReview'] .error"));
+
+
+	//--------------------------------------------------------------------- 
+
+    // 2. PLACEHOLDERS HANDLE PATTERN MODULE
+	(function(){
+
+		var placeholdersHandler = {
+
+			init: function(){
+				// вызов внутренних функций
+				this._setUpListeners();
+			},
+
+			_setUpListeners: function(){
+				$('input, textarea').focus(placeholdersHandler._placeholdersRemoveMethod);
+				$('input, textarea').blur(placeholdersHandler._placeholdersAddMethod);
+			},
+
+			_placeholdersRemoveMethod: function(){
+				placeHolderSave = $(this).attr('placeholder');
+		        $(this).attr('placeholder', '');
+			},
+
+			_placeholdersAddMethod: function(){
+				$(this).attr('placeholder', placeHolderSave);
+			},
+
+		};
+
+		placeholdersHandler.init();
+
+	}());
 	
-	$('input, textarea').on('focus', function(){
-		placeHolderSave = $(this).attr('placeholder');
-		$(this).attr('placeholder', '');
-	});
-
-	$('input, textarea').on('blur', function(){
-		$(this).attr('placeholder', placeHolderSave);
-	});
+	//---------------------------------------------------------------------
 	
-	//---------------------------------------------------------------------
-	// ADMIN PANNEL SMALL SCREEN -- ADMIN TABS
+    // 3. SMALL SCREEN NAVIGATION PATTERN MODULE
+    (function(){
+	    var requiredClass = 'hidden';
 
-	var adminNavOne = $(".admin-tab[id='admin-nav-1']");
-	var adminNavTwo = $(".admin-tab[id='admin-nav-2']");
+	    // NAVIGATION SMALL SCREEN -- NAVIGATION MAIN BUTTONS
 
-	var adminNavBlockOne = $(".admin-nav-single-block[id='admin-nav-block-1']");
-	var adminNavBlockTwo = $(".admin-nav-single-block[id='admin-nav-block-2']");
+	    var buttonForOpen = $(".fa-bars[id='open__navigation__button']");
+	    var buttonForClose = $(".fa-times[id='close__navigation__button']");
 
-	//$(adminNavOne).focus();
+	    var navigationSmallSlide = $(".navigation__slide[id='navigation__slide']");
 
-	$(adminNavTwo).on('click', function(){
-		if ($(adminNavBlockTwo).hasClass(requiredClass) ){
-			$(adminNavBlockTwo).removeClass(requiredClass);
-		    $(adminNavBlockOne).addClass(requiredClass);
-		}				
-	});
+	    // ADMIN PANNEL SMALL SCREEN -- ADMIN TABS
 
-	$(adminNavOne).on('click', function(){
-		if ($(adminNavBlockOne).hasClass(requiredClass) ){
-			$(adminNavBlockOne).removeClass(requiredClass);
-		    $(adminNavBlockTwo).addClass(requiredClass);
-		}		
-	});
-	//---------------------------------------------------------------------
-	// ADMIN PANNEL SMALL SCREEN -- MAIN NAVIGATION BUTTON
+	    var adminNavOne = $(".admin-tab[id='admin-nav-1']");
+	    var adminNavTwo = $(".admin-tab[id='admin-nav-2']");
 
-	var buttonForOpen = $(".fa-bars[id='open__navigation__button']");
-	var buttonForClose = $(".fa-times[id='close__navigation__button']");
+	    var adminNavBlockOne = $(".admin-nav-single-block[id='admin-nav-block-1']");
+	    var adminNavBlockTwo = $(".admin-nav-single-block[id='admin-nav-block-2']");
 
-	var navigationSmallSlide = $(".navigation__slide[id='navigation__slide']");
 
-	$(buttonForOpen).on('click', function(){
-		if ($(navigationSmallSlide).hasClass(requiredClass) ){
-			$(navigationSmallSlide).removeClass(requiredClass);
-		}
-		$(buttonForOpen).addClass(requiredClass);
-	    $(adminNavOne).focus();
-	});
+		var navigationSmallScreen = {
 
-	$(buttonForClose).on('click', function(){
-		$(navigationSmallSlide).addClass(requiredClass);
-		$(buttonForOpen).removeClass(requiredClass);
-			
-	});
-	//---------------------------------------------------------------------
+			init: function(){
+				// вызов внутренних функций
+				this._setUpListeners();
+			},
+
+			_setUpListeners: function(){
+				$(adminNavTwo).click(navigationSmallScreen._adminNavTwoMethod);
+				$(adminNavOne).click(navigationSmallScreen._adminNavOneMethod);
+				$(buttonForOpen).click(navigationSmallScreen._buttonForOpenMethod);
+				$(buttonForClose).click(navigationSmallScreen._buttonForCloseMethod);				
+			},
+
+			_adminNavTwoMethod: function(){
+				if ($(adminNavBlockTwo).hasClass(requiredClass) ){
+			        $(adminNavBlockTwo).removeClass(requiredClass);
+		            $(adminNavBlockOne).addClass(requiredClass);
+		        }				
+			},
+
+			_adminNavOneMethod: function(){
+				if ($(adminNavBlockOne).hasClass(requiredClass) ){
+			        $(adminNavBlockOne).removeClass(requiredClass);
+		            $(adminNavBlockTwo).addClass(requiredClass);
+		        }				
+			},
+
+			_buttonForOpenMethod: function(){
+				if ($(navigationSmallSlide).hasClass(requiredClass) ){
+			        $(navigationSmallSlide).removeClass(requiredClass);
+		        }
+		        $(buttonForOpen).addClass(requiredClass);
+	            $(adminNavOne).focus();				
+			},
+
+			_buttonForCloseMethod: function(){
+				$(navigationSmallSlide).addClass(requiredClass);
+		        $(buttonForOpen).removeClass(requiredClass);
+			},
+
+		};
+
+		navigationSmallScreen.init();
+
+	}());	
 
 
 });
--- app/js/map.js
+++ app/js/map.js
@@ -2,34 +2,62 @@ var map;
 // Функция initMap которая отрисует карту на странице
 function initMap() {
 
+    // Запишем сразу координаты в переменную
+    var coordinates = {lat: 51.3607, lng: 1.0257};
+
+    // Определяем клиент, чтобы после отключить перетаскивание карты на смартфонах.  Часть 1.
+    //var isDraggable;
+    //if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    //    isDraggable = false;
+    //} else {
+    //    isDraggable = true;
+    //}
+
     // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
     map = new google.maps.Map(document.getElementById('map'), {
     // При создании объекта карты необходимо указать его свойства
     // center - определяем точку на которой карта будет центрироваться
-    center: {lat: 51.3607, lng: 1.0257},
+    center: coordinates,
     // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
     zoom: 7,
 
     // Добавляем свои стили для отображения карты
     //styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
-    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}]
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}],
     
+    // Дополнительные настройки
+
+        // Удалить элементы управления картой
+        //disableDefaultUI: true,
+
+        // Запретить увеличение карты по скроллу
+        scrollwheel: false,
+
+        // Отключить перетаскивание для смартфонов. Часть 2.
+        //draggable: isDraggable,
+        //либо
+        draggable: false
+
     });
 
     // Создаем маркер на карте
     var marker = new google.maps.Marker({
 
     // Определяем позицию маркера
-    position: {lat: 51.3607, lng: 1.0257},
+    position: coordinates,
 
     // Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
     map: map,
 
     // Пишем название маркера - появится если навести на него курсор и немного подождать
-    title: 'Whitstable'
+    title: 'Whitstable',
 
     // Укажем свою иконку для маркера
-    //icon: 'theatre_icon.png'
+    icon: '../img/marker-grey.png',
+
+    //Анимация маркера
+    animation: google.maps.Animation.BOUNCE
+
 
     });
     //==========================================================
